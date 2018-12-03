47fc09e6968eed70da7031baa1002556a5a8742e
janoberhansli <jan.oberhaensli@stud.kftg.ch>
2018-12-03 14:21:56 +0100

    Map Update
    Datenlayer werden nun effizienter geladen. Es wird nur noch dieser bei
    einer Veränderung aktualisiert und nicht mehr die ganze Karte.

--- js/map.js
+++ js/map.js
@@ -4,6 +4,9 @@ var dataLayerName = 'entwaesserungsgraben';
 var wmsLayer = '';
 var maplayer = '';
 var legend = L.control({position: 'bottomright'});
+var runningSpinner = false;
+const spinnerMap = document.getElementsByClassName('spinner')[0];
+const spinnerMap1 = document.getElementsByClassName('spinner')[1];
 
 //restrict view of map
 const topLeftCorner = L.latLng(47.7157, 8.6538);
@@ -33,61 +36,98 @@ zoom.addTo(mymap);
 //Show scale meter on bottom left corner
 L.control.scale().addTo(mymap);
 
+
 // Chane map style
 function changeMapStyle(name) {
+
+
     //Remove every layer on the map
-    if(maplayer !== '')
+    if (maplayer !== '')
         mymap.removeLayer(maplayer);
-    if(wmsLayer !== '')
+    if (wmsLayer !== '')
         mymap.removeLayer(wmsLayer);
 
-    //Add new layer to map
-    maplayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: name,
-        format: 'images/png',
-        accessToken: 'pk.eyJ1IjoiamFub2JlMiIsImEiOiJjam00b3Vpa2wzZjNoM3BxbmJtams3Z2U0In0.ZOdhoX3gBfEJkGy0-w8Bwg'
-    }).addTo(mymap);
+    if (dataLayerName !== '') {
+        //Add new layer to map
+        maplayer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            id: name,
+            format: 'images/png',
+            accessToken: 'pk.eyJ1IjoiamFub2JlMiIsImEiOiJjam00b3Vpa2wzZjNoM3BxbmJtams3Z2U0In0.ZOdhoX3gBfEJkGy0-w8Bwg'
+        }).addTo(mymap);
 
+        addSpinner(maplayer);
 
-    //Add data layer over ground layer
-    if(wmsLayer !== '')
-        wmsLayer.addTo(mymap);
+        //Add data layer over ground layer
+        if (wmsLayer !== '')
+            wmsLayer.addTo(mymap);
+    }
 }
 
-function changeLayer(thisId){
+
+function changeLayer(thisId) {
+
     //remove data layer & legend
-    if(wmsLayer !== '')
+    if (wmsLayer !== '')
         mymap.removeLayer(wmsLayer);
-        legend.remove(mymap);
+    legend.remove(mymap);
 
-    //check, if checkbox is checked or not, if it's checked then draw layer
-    if(document.getElementById(thisId).checked)
-        dataLayerName = thisId;
-    else
-        dataLayerName = '';
+    if (dataLayerName !== '') {
 
-    wmsLayer = L.tileLayer.wms('http://map.geo.tg.ch//proxy/geofy_chsdi3/gewaesserkataster_gewaesser-gewaesserlauf?access_key=YoW2syIQ4xe0ccJA&', {
-        version: '1.3.0',
-        format: 'image/png',
-        transparent: true,
-        crs: L.CRS.EPSG4326,
-        opacity: 1,
-        identify: false,
-        layers: dataLayerName
-    }).addTo(mymap);
+        //check, if checkbox is checked or not, if it's checked then draw layer
+        if (document.getElementById(thisId).checked)
+            dataLayerName = thisId;
+        else
+            dataLayerName = '';
 
-    // Legende
-    legend.onAdd = function () {
-        var div = L.DomUtil.create('div', 'info legend');
-        var url = 'http://map.geo.tg.ch//proxy/geofy_chsdi3/gewaesserkataster_gewaesser-gewaesserlauf?access_key=YoW2syIQ4xe0ccJA&version=1.3.0&service=WMS&request=GetLegendGraphic&sld_version=1.1.0&layer=' + dataLayerName + '&format=image/png&STYLE=default';
-        div.innerHTML += '<h4>Legende</h4><br>' +
-            '<img src='+url+' alt="legend" class="img-fluid" style="width: 100%; display: block;">';
+        wmsLayer = L.tileLayer.wms('http://map.geo.tg.ch//proxy/geofy_chsdi3/gewaesserkataster_gewaesser-gewaesserlauf?access_key=YoW2syIQ4xe0ccJA&', {
+            version: '1.3.0',
+            format: 'image/png',
+            transparent: true,
+            crs: L.CRS.EPSG4326,
+            opacity: 1,
+            identify: false,
+            layers: dataLayerName
+        }).addTo(mymap);
 
-        return div;
-    };
-    legend.addTo(mymap);
+        addSpinner(wmsLayer);
+
+        // Legende
+        //If legend shows only one item, then do not display
+        if (dataLayerName === 'entwaesserungsgraben' || dataLayerName === 'fliessgewaesser' || dataLayerName === 'Stehendes_Gewaesser') { //Data with multiple items in legend
+            legend.onAdd = function (mymap) {
+                var div = L.DomUtil.create('div', 'info legend');
+                var url = 'http://map.geo.tg.ch//proxy/geofy_chsdi3/gewaesserkataster_gewaesser-gewaesserlauf?access_key=YoW2syIQ4xe0ccJA&version=1.3.0&service=WMS&request=GetLegendGraphic&sld_version=1.1.0&layer=' + dataLayerName + '&format=image/png&STYLE=default';
+                div.innerHTML += '<h4>Legende</h4><br>' +
+                    '<img src=' + url + ' alt="legend" width="200" height="150">';
+                return div;
+            };
+            legend.addTo(mymap);
+        }
+    }
 }
+
+
+//Add listener
+function addSpinner(layer) {
+    layer.on('tileload', function () {
+        if(!runningSpinner) {
+            spinnerMap.style.display = 'block';
+            spinnerMap1.style.display = 'block';
+            runningSpinner = true;
+        }
+
+    });
+    layer.on('load', function () {
+        if(!layer.isLoading()) {
+            spinnerMap.style.display = 'none';
+            spinnerMap1.style.display = 'none';
+            runningSpinner = false;
+        }
+    });
+}
+
+
 
 //Add Event Listener for radio buttons
 document.getElementById("tgKarteStreets").addEventListener("click", function () {
@@ -105,4 +145,6 @@ document.getElementById("tgKarteSatellite").addEventListener("click", function (
 
 // generate map
 changeMapStyle(mapname);
+
+
 
