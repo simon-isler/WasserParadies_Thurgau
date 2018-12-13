88966f2bd83258812c77355b41300b58120207e3
simon-isler <simon-isler@gmx.ch>
2018-12-13 13:54:18 +0100

    detect if OGD data is online

--- js/map.js
+++ js/map.js
@@ -106,49 +106,84 @@ document.getElementById("tgKarteSatellite").addEventListener("click", function (
     changeMapStyle(mapname);
 });
 
+// function to check if WMS data is online
+function isSiteOnline(url,callback) {
+    // try to load favicon
+    var timer = setTimeout(function(){
+        // timeout after 5 seconds
+        callback(false);
+    },5000);
+
+    var img = document.createElement("img");
+    img.onload = function() {
+        clearTimeout(timer);
+        callback(true);
+    };
+
+    img.onerror = function() {
+        clearTimeout(timer);
+        callback(false);
+    };
+
+    img.src = url+"/favicon.ico";
+}
+
 // change layer
 function changeLayer(thisId) {
-    //remove data layer & legend
-    if (wmsLayer !== '') {
-        map.removeLayer(wmsLayer);
-        legend.remove(map);
-    }
+    // WMS data URL
+    var dataUrl = "https://map.geo.tg.ch/proxy/geofy_chsdi3/gewaesserkataster_gewaesser-gewaesserlauf?access_key=YoW2syIQ4xe0ccJA&Service=WMS&Version=1.3.0&Request=GetCapabilities&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&FORMAT=image%2Fpng&TRANSPARENT=true&LAYERS=" + dataLayerName + "&CRS=EPSG%3A2056&STYLES=&WIDTH=650&HEIGHT=390&BBOX=2690000%2C1246000%2C2755000%2C1285000";
 
-    //check, if checkbox is checked or not, if it's checked then draw layer
-    if (dataLayerName !== '') {
-        if (document.getElementById(thisId).checked) {
-            dataLayerName = thisId;
-        } else {
-            dataLayerName = '';
+    isSiteOnline(dataUrl,function(found){
+        // site is online
+        if(found) {
+            //remove data layer & legend
+            if (wmsLayer !== '') {
+                map.removeLayer(wmsLayer);
+                legend.remove(map);
+            }
+
+            //check, if checkbox is checked or not, if it's checked then draw layer
+            if (dataLayerName !== '') {
+                if (document.getElementById(thisId).checked) {
+                    dataLayerName = thisId;
+                } else {
+                    dataLayerName = '';
+                }
+
+                // add wms layer
+                wmsLayer = L.tileLayer.wms('http://map.geo.tg.ch//proxy/geofy_chsdi3/gewaesserkataster_gewaesser-gewaesserlauf?access_key=YoW2syIQ4xe0ccJA&', {
+                    version: '1.3.0',
+                    format: 'image/png',
+                    transparent: true,
+                    crs: L.CRS.EPSG4326,
+                    opacity: 1,
+                    identify: false,
+                    layers: dataLayerName
+                }).addTo(map);
+
+                // show loading icon
+                addSpinner(wmsLayer);
+
+                // show legend (if legend shows only one item, then do not display)
+                if (dataLayerName === 'entwaesserungsgraben' || dataLayerName === 'fliessgewaesser' || dataLayerName === 'Stehendes_Gewaesser') { //Data with multiple items in legend
+                    legend.onAdd = function () {
+                        // create legend
+                        var div = L.DomUtil.create('div', 'info legend');
+                        var url = 'http://map.geo.tg.ch//proxy/geofy_chsdi3/gewaesserkataster_gewaesser-gewaesserlauf?access_key=YoW2syIQ4xe0ccJA&version=1.3.0&service=WMS&request=GetLegendGraphic&sld_version=1.1.0&layer=' + dataLayerName + '&format=image/png&STYLE=default';
+                        div.innerHTML += '<h4>Legende</h4><br>' +
+                            '<img src=' + url + ' alt="legend" class="legend">';
+                        return div;
+                    };
+                    legend.addTo(map);
+                }
+            }
         }
-
-        // add wms layer
-        wmsLayer = L.tileLayer.wms('http://map.geo.tg.ch//proxy/geofy_chsdi3/gewaesserkataster_gewaesser-gewaesserlauf?access_key=YoW2syIQ4xe0ccJA&', {
-            version: '1.3.0',
-            format: 'image/png',
-            transparent: true,
-            crs: L.CRS.EPSG4326,
-            opacity: 1,
-            identify: false,
-            layers: dataLayerName
-        }).addTo(map);
-
-        // show loading icon
-        addSpinner(wmsLayer);
-
-        // show legend (if legend shows only one item, then do not display)
-        if (dataLayerName === 'entwaesserungsgraben' || dataLayerName === 'fliessgewaesser' || dataLayerName === 'Stehendes_Gewaesser') { //Data with multiple items in legend
-            legend.onAdd = function () {
-                // create legend
-                var div = L.DomUtil.create('div', 'info legend');
-                var url = 'http://map.geo.tg.ch//proxy/geofy_chsdi3/gewaesserkataster_gewaesser-gewaesserlauf?access_key=YoW2syIQ4xe0ccJA&version=1.3.0&service=WMS&request=GetLegendGraphic&sld_version=1.1.0&layer=' + dataLayerName + '&format=image/png&STYLE=default';
-                div.innerHTML += '<h4>Legende</h4><br>' +
-                    '<img src=' + url + ' alt="legend" class="legend">';
-                return div;
-            };
-            legend.addTo(map);
+        // data is offline
+        else {
+            alert("Die WMS-Daten konnten nicht geladen werden!");
         }
-    }
+    });
+
 }
 
 // add loading icon
